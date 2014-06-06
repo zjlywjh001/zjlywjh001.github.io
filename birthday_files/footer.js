c5489edaef210fb31388958ced576d6fa4831765
zjlywjh001 <zjlywjh001@gmail.com>
2014-06-06 12:54:41 +0800

    first commit

---
+++ birthday_files/footer.js
@@ -0,0 +1,19 @@
+document.writeln("<script src='http://s16.cnzz.com/stat.php?id=3747459&web_id=3747459' language='JavaScript' charset='gb2312'></script>");				  
+
+document.writeln('<script type="text/javascript">');
+document.writeln("  var _gaq = _gaq || [];");
+document.writeln("  _gaq.push(['_setAccount', 'UA-11834964-2']);");
+document.writeln("  _gaq.push(['_trackPageview']);");
+document.writeln("  (function() {");
+document.writeln("    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;");
+document.writeln("    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';");
+document.writeln("    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);");
+document.writeln("  })();");
+document.writeln("</script>");
+
+document.writeln("<script type=\"text/javascript\">");
+document.writeln(" var _bdhmProtocol = ((\"https:\" == document.location.protocol) ? \" https://\" : \" http://\");");
+document.writeln(" document.write(unescape(\"%3Cscript src=\'\" + _bdhmProtocol + \"hm.baidu.com/h.js%3F41fa78d83871255df97eca42c516d7a6\' type=\'text/javascript\'%3E%3C/script%3E\"));");
+document.writeln(" </script>");
+
+//document.domain="szhufu.com";
